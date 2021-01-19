a346b0346e894ef6c9a2b977ec24e70d1f6e2ced
Skiadramm <72439326+Skiadramm@users.noreply.github.com>
2021-01-19 14:17:31 +0100

    Commentaires des classe Commande.h et Client.h
    Mise à jour des commentaires

--- Client.h
+++ Client.h
@@ -8,36 +8,38 @@
 #ifndef _CLIENT_H
 #define _CLIENT_H
 
-#include <iostream>
-#include <string>
-#include <vector>
-#include "Produit.h"
+#include <iostream> // on inclut dans la classe la bibliothèque iostream
+#include <string>	// on inclut dans la classe la bibliothèque string
+#include <vector>	// on inclut dans la classe la bibliothèque vector
+#include "Produit.h"// on lie le .cpp à la classe qui le définit
+
+//Creation et définition de la classe Client
 
 class Client{
 
 public:
 
-	Client(std::string nom, std::string prenom, int id);
+	Client(std::string nom, std::string prenom, int id);	// construteur contenant tous les membres appartenant à private
 
-	int getID();
-	std::string getPrenom();
-	std::string getNom();
-	std::vector<Produit*> getPanier();
+	int getID(); // Getteur permettant de récupérer l'id du client
+	std::string getPrenom(); // Getteur permettant de récupérer le prénom du client
+	std::string getNom();	// Getteur permettant de récupérer le nom du client
+	std::vector<Produit*> getPanier(); // Getteur permettant de récupérer le panier du client
 
-	void addtoCart(Produit* produit);
-	void clearCart();
-	void changeQuantity(std::string titre, int quantite);
-	void deleteProduit(std::string titre);
+	void addtoCart(Produit* produit);	// fonction permettant d'ajouter un produit au panier
+	void clearCart();	// fonction permettant de vider le panier
+	void changeQuantity(std::string titre, int quantite);	// fonction permettant de changer la quantité d'un item dans le panier
+	void deleteProduit(std::string titre);	// fonction permettant de supprimer un produit du panier
 
 private:
 
-	int _id;
-	std::string _prenom;
-	std::string _nom;
-	std::vector<Produit*> _panier;
+	int _id;	// variable de type int pour l'ID
+	std::string _prenom; 	// variable de type string pour le prénom du client
+	std::vector<Produit*> _panier;	// variable de type vector contenant un pointeur "Produit*" pour le panier du client
 
 };
 
-std::ostream& operator << (std::ostream& output, Client& obj);
+
+std::ostream& operator << (std::ostream& output, Client& obj);	// Surcharge de l'operateur <<
 
 #endif
--- Commande.h
+++ Commande.h
@@ -8,31 +8,33 @@
 #ifndef COMMANDE_H
 #define COMMANDE_H
 
-#include <vector>
-#include "Client.h"
-#include "Produit.h"
+#include <vector> 	// on inclut dans la classe la bibliothèque vector
+#include "Client.h" 	// on inclut dans la classe la classe Client.h
+#include "Produit.h"	// on inclut dans la classe la classe Produit.h
+
+//Creation et définition de la classe Commande
 
 class Commande{
 
 public:
 
-	Commande(Client* client, std::vector<Produit*> panier, int numero);
+	Commande(Client* client, std::vector<Produit*> panier, int numero);	// construteur contenant tous les membres appartenant à private
 
-	Client* getClient();
-	std::vector<Produit*> getPanier();
-	std::string getStatus();
-	int getNumero();
+	Client* getClient();	// Getteur permettant de récupérer les infos du client
+	std::vector<Produit*> getPanier();	// Getteur permettant de récupérer le panier du client pour la commande
+	std::string getStatus();	// Getteur permettant de récupérer le statut de la commande
+	int getNumero();	// Getteur permettant de récupérer le numéro de la commande
 
-	void setStatus(std::string etat);
+	void setStatus(std::string etat); // Setteur permettant de déterminer et/ou changer l'état de la commande
 
 private:
 
-	int _numero;
-	Client* _client;
-	std::vector<Produit*> _panier;
-	std::string _status;
+	int _numero;	// variable de type int pour le numéro de la commande
+	Client* _client;	// variable de type Client* pour le client
+	std::vector<Produit*> _panier;	// variable de type vector contenant le pointeur Produit* pour le panier
+	std::string _status; 	// variable de type string pour le statut de la commande
 
 };
 
-std::ostream& operator << (std::ostream& output, Commande& obj);
+std::ostream& operator << (std::ostream& output, Commande& obj);	// Surcharge de l'operateur <<
 #endif
